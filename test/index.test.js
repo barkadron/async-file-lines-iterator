1cea8e565c9ed86f0d144e426392135c872438e3
Andrey Barkalov <barkadron@mail.ru>
2019-08-14 20:05:58 +0300

    Add more tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,88 @@
 import chai from 'chai';
 import { generateLines, createTestFile } from './setupTestEnv';
+
 import getAsyncFileLinesIterator from '../src/index';
 
-describe('getAsyncFileLinesIterator', function() {
+describe('getAsyncFileLinesIterator:', function() {
     it('must be a function.', function() {
         chai.assert.isFunction(getAsyncFileLinesIterator);
     });
 
-    it('must read all lines from file.', async function() {
-        const lines = generateLines();
-        const file = createTestFile({ lines });
-        const readLines = [];
-
+    it('must return async-iterable object.', function() {
+        const file = createTestFile();
         const linesIterator = getAsyncFileLinesIterator(file);
-        // let i = 0;
-        for await (const line of linesIterator) {
-            readLines.push(line);
-            // console.log(`--> ${line}`);
-            // chai.assert.equal(line, lines[i++]);
-        }
-        chai.assert.equal(readLines.length, lines.length);
+        chai.assert.equal(typeof linesIterator[Symbol.asyncIterator], 'function');
     });
 
-    // @TODO: https://mochajs.org/#dynamically-generating-tests
+    describe('asyncFileLinesIterator:', function() {
+        const lines = generateLines({ linesCount: 500 });
+        const file = createTestFile({ lines });
+
+        describe(`for test-file with ${lines.length} lines and size = ${file.size} bytes:`, function() {
+            const linesLimits = [
+                0,
+                1,
+                Math.floor(lines.length / 2),
+                lines.length - 1,
+                lines.length,
+                lines.length + 1,
+                undefined,
+            ];
+
+            const chunkSizes = [
+                0,
+                50,
+                Math.floor(file.size / 2),
+                file.size - 1,
+                file.size,
+                file.size + 1,
+                undefined,
+            ];
+
+            const tests = [];
+            chunkSizes.forEach(chunkSize => {
+                linesLimits.forEach(linesLimit => {
+                    tests.push({
+                        args: { chunkSize, linesLimit },
+                        expected:
+                            linesLimit === undefined || linesLimit >= lines.length
+                                ? lines.length
+                                : linesLimit,
+                    });
+                });
+            });
+
+            for (let i = 0; i < tests.length; i++) {
+                const test = tests[i];
+                const readLines = [];
+                const linesIterator = getAsyncFileLinesIterator(file, {
+                    linesLimit: test.args.linesLimit,
+                    chunkSize: test.args.chunkSize,
+                });
+
+                const message = `must read ${
+                    test.args.linesLimit === test.expected ? test.expected : 'all'
+                } lines if 'linesLimit' = ${
+                    test.args.linesLimit === undefined || test.args.linesLimit <= test.expected
+                        ? test.args.linesLimit
+                        : '<more than exists>'
+                } and 'chunkSize' = ${
+                    test.args.chunkSize === undefined || test.args.chunkSize <= file.size
+                        ? test.args.chunkSize
+                        : '<more than file.size>'
+                }`;
+
+                // https://mochajs.org/#dynamically-generating-tests
+                it(message, async function() {
+                    let lineNum = 0;
+                    for await (const line of linesIterator) {
+                        readLines.push(`${line}${lineNum + 1 !== lines.length ? '\n' : ''}`);
+                        chai.assert.equal(lines[lineNum], readLines[lineNum]);
+                        lineNum++;
+                    }
+                    chai.assert.equal(readLines.length, test.expected);
+                });
+            }
+        });
+    });
 });
--- test/setupTestEnv.js
+++ test/setupTestEnv.js
@@ -12,27 +12,20 @@ Object.defineProperties(global, {
     ...Object.getOwnPropertyDescriptors(global),
 });
 
-export function checkAsync(done, check) {
-    let err = null;
-    try {
-        check();
-    } catch (e) {
-        err = e;
-    } finally {
-        done(err);
-    }
-}
-
-export function generateLines({ lineText = 'x', lineLength = 100, linesCount = 10 } = {}) {
-    const line = lineText.repeat(lineLength);
+export function generateLines({ lineLength = 100, linesCount = 10 } = {}) {
+    const lineText = 'x';
+    const spacer = '__';
+    const lineEnd = '\n';
+    const line = lineText.repeat(
+        lineLength - spacer.length - linesCount.toString().length - lineEnd.length
+    );
     return Array.from(Array(linesCount).fill(line), (elem, index) => {
         const lineNumber = (index + 1).toString().padStart(linesCount.toString().length, '0');
-        const lineBreak = index !== linesCount - 1 ? '\n' : '';
-        return `${lineNumber}__${elem}${lineBreak}`;
+        return `${lineNumber}${spacer}${elem}${index !== linesCount - 1 ? lineEnd : ''}`;
     });
 }
 
-export function createTestFile({ lines = [] }) {
+export function createTestFile({ lines = [] } = {}) {
     const fileName = 'test-file.txt';
     const fileType = 'text/plain';
     return new File(lines, fileName, { type: fileType });
